ee94ef732981fe6d27b5deab415d7c886f4dbc91
Kevin Williams <kevin.williams@kevinwilliams.dev>
2024-09-23 13:12:30 -0500

    wrap all migrations in a transaction

--- hyperion/lib/services/data_service/migrations/run_migrations.dart
+++ hyperion/lib/services/data_service/migrations/run_migrations.dart
@@ -3,6 +3,8 @@ import 'package:hyperion/services/data_service/migrations/v2.dart';
 import 'package:sqflite/sqflite.dart';
 
 runMigrations(Database db, int dbVersion) async {
-  if (dbVersion > 0) await version1(db);
-  if (dbVersion > 1) await version2(db);
+  await db.transaction((tx) async {
+    if (dbVersion > 0) await version1(tx);
+    if (dbVersion > 1) await version2(tx);
+  });
 }
--- hyperion/lib/services/data_service/migrations/v1.dart
+++ hyperion/lib/services/data_service/migrations/v1.dart
@@ -2,51 +2,49 @@ import 'package:sqflite/sqlite_api.dart';
 
 // 9/22/24
 // Create an initial version and settings table
-version1(Database db) async {
-  await db.transaction((tx) async {
-    await tx.execute('''
-      CREATE TABLE IF NOT EXISTS Version (id INTEGER PRIMARY KEY AUTOINCREMENT, date DATETIME);
-    ''');
+version1(Transaction tx) async {
+  await tx.execute('''
+    CREATE TABLE IF NOT EXISTS Version (id INTEGER PRIMARY KEY AUTOINCREMENT, date DATETIME);
+  ''');
 
-    await tx.execute('''
-      CREATE TABLE IF NOT EXISTS Settings (id INTEGER PRIMARY KEY AUTOINCREMENT, key TEXT, value TEXT);
-    ''');
+  await tx.execute('''
+    CREATE TABLE IF NOT EXISTS Settings (id INTEGER PRIMARY KEY AUTOINCREMENT, key TEXT, value TEXT);
+  ''');
 
-    // Create a temp table so I can insert all values at once, instead of individual inserts
-    await tx.execute('''
-      CREATE TEMP TABLE IF NOT EXISTS TempSettings (id INTEGER PRIMARY KEY AUTOINCREMENT, key TEXT, value TEXT);
-    ''');
+  // Create a temp table so I can insert all values at once, instead of individual inserts
+  await tx.execute('''
+    CREATE TEMP TABLE IF NOT EXISTS TempSettings (id INTEGER PRIMARY KEY AUTOINCREMENT, key TEXT, value TEXT);
+  ''');
 
-    await tx.execute('''
-      INSERT INTO TempSettings(id, key, value)
-      VALUES
-      (1, 'mqtt.host', ''),
-      (2, 'mqtt.port', ''),
-      (3, 'mqtt.client', ''),
-      (4, 'mqtt.username', ''),
-      (5, 'mqtt.password', ''),
-      (6, 'mqtt.channels.notifications', '')
-    ''');
+  await tx.execute('''
+    INSERT INTO TempSettings(id, key, value)
+    VALUES
+    (1, 'mqtt.host', ''),
+    (2, 'mqtt.port', ''),
+    (3, 'mqtt.client', ''),
+    (4, 'mqtt.username', ''),
+    (5, 'mqtt.password', ''),
+    (6, 'mqtt.channels.notifications', '')
+  ''');
 
-    await tx.execute('''
-      INSERT INTO Settings(id, key, value)
-      SELECT t.id, t.key, t.value
-      FROM TempSettings t
-      WHERE NOT EXISTS (
-        SELECT 1
-        FROM Settings s
-        WHERE s.id = t.id
-      )
-    ''');
+  await tx.execute('''
+    INSERT INTO Settings(id, key, value)
+    SELECT t.id, t.key, t.value
+    FROM TempSettings t
+    WHERE NOT EXISTS (
+      SELECT 1
+      FROM Settings s
+      WHERE s.id = t.id
+    )
+  ''');
 
-    await tx.execute('''
-      INSERT INTO Version (id, date)
-      SELECT 1, DATETIME('now')
-      WHERE NOT EXISTS (
-        SELECT 1
-        FROM Version
-        WHERE id = 1
-      );
-    ''');
-  });
+  await tx.execute('''
+    INSERT INTO Version (id, date)
+    SELECT 1, DATETIME('now')
+    WHERE NOT EXISTS (
+      SELECT 1
+      FROM Version
+      WHERE id = 1
+    );
+  ''');
 }
